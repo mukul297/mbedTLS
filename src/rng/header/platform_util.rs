cd6300ab7a30f33059e845c78193f3c83361d22d
Mukul Sharma <43707359+mukul297@users.noreply.github.com>
2021-03-05 11:42:53 +0530

    Add files via upload

---
+++ src/rng/header/platform_util.rs
@@ -0,0 +1,10 @@
+/**
+ * \file platform_util.h
+ *
+ * \brief Common and shared functions used by multiple modules in the Mbed TLS
+ *        library.
+ */
+
+/* 
+ * NO STRUCTURE OR DATA TYPE TO DECLARE
+ */
